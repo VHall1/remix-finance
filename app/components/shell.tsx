658498cfcb699ef0c36f4f9fb7da57f4e6c6bf8b
VHall1 <victorhallpsn@hotmail.com>
2024-04-03 19:32:41 +0100

    fix shell height

--- app/components/shell.tsx
+++ app/components/shell.tsx
@@ -7,7 +7,7 @@ import { handle as rootHandle } from "~/root";
 
 export function Shell({ children }: PropsWithChildren) {
   return (
-    <div className="grid h-screen [grid-template-columns:minmax(max-content,12.8125rem)_1fr] [grid-template-rows:max-content_max-content_1fr_max-content] [grid-template-areas:'logo_header''aside_main']">
+    <div className="grid h-screen [grid-template-columns:minmax(max-content,12.8125rem)_1fr] [grid-template-rows:max-content_1fr] [grid-template-areas:'logo_header''aside_main']">
       <Link
         to={rootHandle.path()}
         className="border-b border-r bg-gray-100/40 px-6 dark:bg-gray-800/40 [grid-area:logo] flex items-center gap-2 font-semibold"
@@ -16,7 +16,7 @@ export function Shell({ children }: PropsWithChildren) {
         Remix Finance
       </Link>
       <Navbar />
-      <nav className="grid items-start px-4 text-sm font-medium h-screen border-r bg-gray-100/40 lg:block dark:bg-gray-800/40 [grid-area:aside]">
+      <nav className="grid items-start px-4 text-sm font-medium border-r bg-gray-100/40 lg:block dark:bg-gray-800/40 [grid-area:aside]">
         <div className="flex-1 overflow-auto py-2">
           <Link
             className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
