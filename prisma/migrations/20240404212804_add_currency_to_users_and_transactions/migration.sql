7adc662993800b2d842733fc32dbdcaac812b1ef
VHall1 <victorhallpsn@hotmail.com>
2024-06-11 21:34:52 +0100

    reset database

--- prisma/migrations/20240404212804_add_currency_to_users_and_transactions/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "Currency" AS ENUM ('CAD', 'EUR', 'GBP', 'USD');
-
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "currency" "Currency";
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "defaultCurrency" "Currency" NOT NULL DEFAULT 'GBP';
---
+++ prisma/migrations/20240611203355_init/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "Currency" AS ENUM ('CAD', 'EUR', 'GBP', 'USD');
+
+-- CreateEnum
+CREATE TYPE "TransactionDirection" AS ENUM ('INBOUND', 'OUTBOUND');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "avatar" TEXT,
+    "passwordHash" TEXT NOT NULL,
+    "defaultCurrency" "Currency" NOT NULL DEFAULT 'GBP',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "reference" TEXT,
+    "direction" "TransactionDirection" NOT NULL,
+    "currency" "Currency" NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
