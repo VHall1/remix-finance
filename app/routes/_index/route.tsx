4c3b9eee83018e298b286fe7e20ac652ff48e3d5
VHall1 <victorhallpsn@hotmail.com>
2024-06-13 00:36:05 +0100

    tweal recent transactions

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,22 +1,16 @@
+import { parseWithZod } from "@conform-to/zod";
 import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { InboxIcon, PlusIcon } from "lucide-react";
+import { ChevronRightIcon, InboxIcon } from "lucide-react";
 import { Shell } from "~/components/shell";
 import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { addAccountSchema } from "~/schemas/account";
+import { addAccount } from "~/services/account.server";
 import { db } from "~/utils/db.server";
 import { formatMoney } from "~/utils/money";
 import { requireUser } from "~/utils/session.server";
 import { AccountsCard } from "./accounts-card";
-import { parseWithZod } from "@conform-to/zod";
-import { addAccountSchema } from "~/schemas/account";
-import { addAccount } from "~/services/account.server";
 
 export default function Dashboard() {
   const { recentTransactions } = useLoaderData<typeof loader>();
@@ -27,54 +21,38 @@ export default function Dashboard() {
         <AccountsCard />
         <Card>
           <CardHeader className="pb-0">
-            <div className="flex justify-between">
-              <div className="grid gap-1.5">
-                <CardTitle>Recent Activity</CardTitle>
-                <CardDescription>
-                  <Button variant="link" size="sm" className="p-0 h-4">
-                    All Transactions
-                  </Button>
-                </CardDescription>
-              </div>
-              <Button size="sm">
-                <PlusIcon />
-                Transaction
+            <div className="flex justify-between items-center">
+              <CardTitle>Recent Activity</CardTitle>
+              <Button variant="link" size="sm" className="p-0">
+                See all
+                <ChevronRightIcon className="h-4 w-4 ml-1" />
               </Button>
             </div>
           </CardHeader>
           {recentTransactions.length > 0 ? (
-            <>
-              <hr className="mt-5" />
-              <CardContent className="px-0 pb-3">
-                <div className="grid">
-                  {recentTransactions.map((transaction) => (
-                    <Button
-                      variant="ghost"
-                      className="h-14 py-0 px-6 rounded-none flex justify-between"
-                      key={transaction.id}
-                    >
-                      <div className="font-semibold">
-                        {transaction.reference}
-                      </div>
-                      <p>{formatMoney(transaction.amount / 100)}</p>
-                    </Button>
-                  ))}
-                </div>
-              </CardContent>
-            </>
+            <CardContent className="pt-3 px-0 pb-3">
+              <div className="grid">
+                {recentTransactions.map((transaction) => (
+                  <Button
+                    variant="ghost"
+                    className="h-14 py-0 px-6 rounded-none flex justify-between"
+                    key={transaction.id}
+                  >
+                    <div className="font-semibold">{transaction.reference}</div>
+                    <p>{formatMoney(transaction.amount / 100)}</p>
+                  </Button>
+                ))}
+              </div>
+            </CardContent>
           ) : (
-            <>
-              <hr className="my-5" />
-              <CardContent>
-                <div className="flex flex-col items-center justify-center space-y-4">
-                  <InboxIcon className="h-12 w-12 text-muted-foreground" />
-                  <h2 className="text-2xl text-center font-semibold text-muted-foreground">
-                    You don&apos;t have any transactions yet!
-                  </h2>
-                  <Button>Add a transaction</Button>
-                </div>
-              </CardContent>
-            </>
+            <CardContent className="pt-6">
+              <div className="flex flex-col items-center justify-center space-y-4 py-4">
+                <InboxIcon className="h-12 w-12 text-muted-foreground" />
+                <h2 className="text-2xl text-center font-semibold text-muted-foreground">
+                  No transactions yet!
+                </h2>
+              </div>
+            </CardContent>
           )}
         </Card>
       </div>
