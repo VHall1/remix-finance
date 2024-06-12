64232d8df39f486065409f3186c69d847f5902a4
VHall1 <victorhallpsn@hotmail.com>
2024-06-12 20:35:51 +0100

    add contrainer class to auth layout wrapper

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function AuthLayout() {
   return (
-    <div className="h-screen flex flex-col justify-center w-full max-w-md mx-auto">
+    <div className="h-screen flex flex-col justify-center w-full container max-w-lg">
       <Outlet />
     </div>
   );
